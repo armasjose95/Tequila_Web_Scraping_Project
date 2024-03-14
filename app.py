ef40cb222a05e9d904f638bb62cc20cf7cdc884d
Jose Armas <armasjose95@gmail.com>
2024-03-13 22:44:41 -0500

    printing links for alcohol products

--- app.py
+++ app.py
@@ -8,16 +8,17 @@ headers = [{
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
 }]
 
-
-page = requests.get(
-    'https://mysa.wine/collections/orange-natural-wine-online')
-soup = BeautifulSoup(page.content, 'html.parser')
+for x in range(1, 4):
+    page = requests.get(
+        f'https://mysa.wine/collections/natural-wine-store?page={x}')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
 
 productList = soup.findAll('div', class_="collection__products")
 
 # print(productList)
 
+wineLinks = []
 # Iterate over each product in productList to find the 'price' class within each
 for product in productList:
     price = product.find_all(class_='price')  # Adjust the class name as needed
@@ -26,3 +27,10 @@ for product in productList:
 
 #price = productList.find('price')
 # print(price)
+
+
+for wine in productList:
+    for link in wine.find_all('a', href=True):
+        wineLinks.append(url + link['href'])
+
+print(len(wineLinks))

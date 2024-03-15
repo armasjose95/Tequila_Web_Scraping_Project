4fa7d3fcc64ff372bb084c52f76d21993f32eaf2
Jose Armas <armasjose95@gmail.com>
2024-03-14 21:55:00 -0500

    debugged printing error

--- app.py
+++ app.py
@@ -8,29 +8,27 @@ headers = [{
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
 }]
 
+wineLinks = []
 for x in range(1, 4):
     page = requests.get(
         f'https://mysa.wine/collections/natural-wine-store?page={x}')
     soup = BeautifulSoup(page.content, 'html.parser')
 
+    productList = soup.findAll('div', class_="collection__products")
 
-productList = soup.findAll('div', class_="collection__products")
+    # print(productList)
 
-# print(productList)
+    # Iterate over each product in productList to find the 'price' class within each
+    for product in productList:
+        # Adjust the class name as needed
+        price = product.find_all(class_='price')
+        print(price)
 
-wineLinks = []
-# Iterate over each product in productList to find the 'price' class within each
-for product in productList:
-    price = product.find_all(class_='price')  # Adjust the class name as needed
-    print(price)
+    #price = productList.find('price')
+    # print(price)
 
-
-#price = productList.find('price')
-# print(price)
-
-
-for wine in productList:
-    for link in wine.find_all('a', href=True):
-        wineLinks.append(url + link['href'])
-
+    for wine in productList:
+        for link in wine.find_all('a', href=True):
+            wineLinks.append(url + link['href'])
+print(wineLinks)
 print(len(wineLinks))

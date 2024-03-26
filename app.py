69370780a2be32d6844b3f5d60ccf418d8764802
Jose Armas <armasjose95@gmail.com>
2024-03-25 23:13:31 -0500

    troubleshooting attribute error

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
 }
 
-wineLinks = []
+wineLinks = set()
 for x in range(1, 3):
     page = requests.get(
         f'https://mysa.wine/collections/natural-wine-store?page={x}')
@@ -27,23 +27,25 @@ for x in range(1, 3):
 
     for wine in productList:
         for link in wine.find_all('a', href=True):
-            wineLinks.append(url + link['href'])
+            wineLinks.add(url + link['href'])
 
 # print(wineLinks)
 # print(len(wineLinks))
 
 
 #testLink = 'https://mysa.wine/products/les-vins-pirouettes-brutal-de-claude-blanc'
+
 for link in wineLinks:
     page = requests.get(link, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
+
     name = soup.find('h1', class_="product__title heading-size-9").text.strip()
     price = soup.find(
         'span', class_='product__price accent-size-5').text.strip()
     description = soup.find('div', class_="tab-content__inner").text.strip()
-    drink = {
+    """drink = {
         'name': name,
         'price': price,
         'description': description
-    }
-    print(drink)
+    }"""
+    print(name)
